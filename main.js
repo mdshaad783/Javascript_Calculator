1669a34065881802aeaa2d77f91ca6091672cf2b
Md Shaad Rehan <113508620+mdshaad783@users.noreply.github.com>
2023-10-09 00:20:53 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+        var string = document.getElementById('screen').innerHTML;
+        function myfun1(){
+            string=string.replace(string ,'')
+            document.getElementById("screen").innerHTML=string;
+        }
+        function myfun(paravalue){
+            if(paravalue== 'd'){
+                len = string.length;
+                string = string.replace(string.charAt(len-1),'')
+                document.getElementById("screen").innerHTML=string;
+            }
+            else if(paravalue == 'eq'){
+                string=eval(string);
+                document.getElementById("screen").innerHTML=string;
+
+            }
+            else{
+            string = string + document.getElementById(paravalue).innerHTML;
+            console.log(string)
+            document.getElementById("screen").innerHTML = string;}
+        }
+    
